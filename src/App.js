b431d959c96f7f97534bb7bef06e1e5526ba7bd4
Dan0804 <wheogus185@naver.com>
2022-02-17 10:35:18 +0900

    220217 learned deep copy, component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '여자 코트 추천', '유아 코트 추천']);
+  let [good, good_count] = useState(0)
+
+  function 제목변경 () {
+    var newArray = [...글제목];
+    newArray[0] = '여자 코트 추천'
+    글제목변경(newArray);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='black-nav'>
+        <div>개발 blog</div>
+      </div>
+      <button onClick={ 제목변경 }>전환 버튼</button>
+        
+      <div className='list'>
+        <h3> {글제목[0]} <span onClick={ () => {good_count(good + 1)} }>👍</span> {good} </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className='list'>
+        <h3> {글제목[1]} </h3>
+        <p>2월 17일 발행</p>
+        <hr/>  
+      </div>
+      <div className='list'>
+        <h3> {글제목[2]} </h3>
+        <p>2월 17일 발행</p>
+        <hr/>  
+      </div>
+
+      <Model/>
+
     </div>
   );
+}
+
+function Model() {
+  return (
+    <div className='model'>
+      <h2>제목</h2>
+      <p>날짜</p>
+      <p>상세 내용</p>
+    </div>
+  )
 }
 
 export default App;
